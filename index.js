f38121ec1dfe768e082a59f4590e3ef431413bc3
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-11-15 04:15:48 +0000

    create next app

--- index.js
+++
@@ -1,8 +0,0 @@
-// https://zeit.co/docs/v2/serverless-functions/introduction/
-module.exports = (req, res) => {
-  res.json({
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies
-  })
-}
