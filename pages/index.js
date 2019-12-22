5ebe4f97af1d262e57f01b720b6c368aaf747813
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-12-22 05:28:03 +0000

    fix import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { PREFIX_NEW_SITE } from "../../constants";
+import { PREFIX_NEW_SITE } from "../constants";
 
 export default class extends React.Component {
   static async getInitialProps({ query, res }) {
